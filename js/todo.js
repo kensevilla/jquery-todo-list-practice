3275dda20ec0033ec3cc106c282ba46effe88ff0
Ken Sevilla <ken.michael.sevilla@oocl.com>
2019-10-02 07:36:09 +0800

    jquery exercise

--- js/todo.js
+++ js/todo.js
@@ -22,4 +22,52 @@ $(document)
         }
 
         // code to be implemented
-    });
+		$(".selectActive").click(function(){
+				$(".checked").hide();
+				$(".unchecked").show();
+			});
+		
+		
+		$(".selectComplete").click(function(){
+				$(".checked").show();
+				$(".unchecked").hide();
+			});
+
+		
+		$(".selectAll").click(function(){
+				$(".checked").show();
+				$(".unchecked").show();
+			});
+			
+		
+		$("#button").click(function(){
+				var currentId = generateUUID;
+				$(".container ol").append('<li id="tempId" class="unchecked"><input name="done-todo" type="checkbox" class="done-todo"></li>');
+		$("#tempId").append('<span>' + $("#item").val() + '</span>');
+				$("#tempId").attr("id", currentId);
+				
+		});
+			
+		$(document).on("click", ".done-todo", function(event){
+			if($(this).parent()[0].className == "unchecked"){
+				$(this).parent()[0].className = "checked";
+			}
+			else{
+				$(this).parent()[0].className = "unchecked";
+			}
+		});
+		
+		$(document).on("dblclick", "li", function(){
+			var list = $(this)
+                .children('span')
+                .attr('contentEditable', 'true')
+                .focus();
+			list.keypress(function(event){
+				if(event.keyCode == 13){
+					list.html(list.text());
+					list.attr('contentEditable', 'false');
+				}
+			});
+		});
+	});
+		
